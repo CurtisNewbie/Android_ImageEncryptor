7b701a37b37c8a8c1da715a89c7bcb57229c7b53
Curtis <Zhuangyongj@gmail.com>
2019-07-13 01:53:01 +0100

    Create Image class that represents a Image object
    A Image object will consist of name, encrypted binary data and so on.

---
+++ app/src/main/java/com/curtisnewbie/ImageItem/Image.java
@@ -0,0 +1,22 @@
+package com.curtisnewbie.ImageItem;
+
+import java.util.Date;
+
+public class Image {
+
+    private byte[] encryptedData;
+    private String name;
+
+    public Image(byte[] data, String name, Date date) {
+        this.encryptedData = data;
+        this.name = name;
+    }
+
+    public byte[] getData() {
+        return this.encryptedData;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
