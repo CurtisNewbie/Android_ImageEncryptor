e7515d46bf56277f0f00351efd89a113208eee1a
Curtis <Zhuangyongj@gmail.com>
2019-07-13 20:13:50 +0100

    Update Account class for testing login credentials

--- app/src/main/java/com/curtisnewbie/account/Account.java
+++ app/src/main/java/com/curtisnewbie/account/Account.java
@@ -6,5 +6,7 @@ public class Account {
     private char[] password;
     private Authorisation auth;
 
-
+    public static final String TEST_NAME = "admin";
+    public static final String TEST_PW = "password";
+    public static final char[] TEST_PW_CHAR = new char[]{'p', 'a', 's', 's', 'w', 'o', 'r', 'd'};
 }
