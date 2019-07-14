3887bc9bbe038d16a34e11684f36eb8c0fcc3ee3
Curtis <Zhuangyongj@gmail.com>
2019-07-14 16:22:44 +0100

    ImageData as a db table: Room Persistence API

---
+++ app/src/main/java/com/curtisnewbie/database/ImageData.java
@@ -0,0 +1,34 @@
+package com.curtisnewbie.database;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "image_table")
+public class ImageData {
+
+    public static final String IMG_TABLE_QUERY = "CREATE TABLE image_table (image_name TEXT PRIMARY KEY, image_data BLOB);";
+
+    @PrimaryKey
+    private String image_name;
+
+    @ColumnInfo(name = "image_data")
+    private String image_data;
+
+
+    public String getImage_name() {
+        return image_name;
+    }
+
+    public void setImage_name(String image_name) {
+        this.image_name = image_name;
+    }
+
+    public String getImage_data() {
+        return image_data;
+    }
+
+    public void setImage_data(String image_data) {
+        this.image_data = image_data;
+    }
+}
