35494f2dadbe34e1d3b6f500842059b963566b3b
Curtis <Zhuangyongj@gmail.com>
2019-07-14 01:16:35 +0100

    Update ImageViewActivity for decryption

--- app/src/main/java/com/curtisnewbie/androidDev/ImageViewActivity.java
+++ app/src/main/java/com/curtisnewbie/androidDev/ImageViewActivity.java
@@ -4,11 +4,15 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.curtisnewbie.ImageItem.Image;
 import com.curtisnewbie.database.DataStorage;
 import com.curtisnewbie.database.DatabaseHelper;
+
+import java.io.IOException;
 
 /**
  * Show the image that is selected from the ImageListActivity
@@ -28,14 +32,21 @@ public class ImageViewActivity extends AppCompatActivity {
         db = DataStorage.getInstance(null).getDB();
         String imageName = getIntent().getStringExtra(ImageListAdapter.IMG_TITLE);
 
-        // get the data from the previous activity
-        byte[] data = db.getEncryptedImgData(imageName);
+        // get the data from the database
+        byte[] encryptedData = db.getEncryptedImgData(imageName);
 
-        // show image
-        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        imageView.setImageBitmap(bitmap);
+        // decrypt the data
+        Image image = new Image(encryptedData);
+        try {
+            byte[] data = image.decrypt(db.getDecryPW());
 
+            // show image
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            Toast.makeText(this, "Decryption Failed", Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
