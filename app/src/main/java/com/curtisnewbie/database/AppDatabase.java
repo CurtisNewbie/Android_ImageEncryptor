62e0f8ee31433a2cf62e94ae1c783ae7d8f949f5
Curtis <Zhuangyongj@gmail.com>
2019-07-14 16:21:31 +0100

    Create AppDatabase as the db for implementing Room Persistence API

---
+++ app/src/main/java/com/curtisnewbie/database/AppDatabase.java
@@ -0,0 +1,17 @@
+package com.curtisnewbie.database;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+/**
+ * This is the database implementing Room api
+ */
+@Database(entities = {Credential.class, ImageData.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract DaoInterface daoInterface();
+}
