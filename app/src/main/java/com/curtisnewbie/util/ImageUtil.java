3fe3340315170fadbad110b41a1e3a3e75439390
CurtisNewbie <zhuangyongj@gmail.com>
2020-04-25 16:05:02 +0800

    Update ImageUtil class
    - Remove unrelated methods
    - Add methods for decoding

--- app/src/main/java/com/curtisnewbie/util/ImageUtil.java
+++ app/src/main/java/com/curtisnewbie/util/ImageUtil.java
@@ -1,110 +1,47 @@
 package com.curtisnewbie.util;
 
-import android.util.Log;
-
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 /**
- * Util Class for image encryption and decryption
+ * Util Class for image decoding
  */
 public class ImageUtil {
 
-    /*
-     * Specification of the encryption and decryption
+    /**
+     * Decode the bytes into bitmap based on the required size.
+     *
+     * @param data      image data
+     * @param reqWidth  required width
+     * @param reqHeight required height
+     * @return bitmap that is decoded.
      */
-    protected static final String HASHING_ALGORITHM = "SHA-256";
-    protected static final String ENCRYPTION_STANDARD = "AES";
+    public static Bitmap decodeBitmapWithScaling(byte[] data, int reqWidth, int reqHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        // for getting the outWidth and outHeight
+        int imgWidth = options.outWidth;
+        int imgHeight = options.outHeight;
+
+        // sub-sampling if necessary
+        int inSampleSize = 1;
+        while ((imgWidth / inSampleSize) > reqWidth || (imgHeight / inSampleSize) > reqHeight) {
+            // always set to the power of 2
+            inSampleSize = inSampleSize == 1 ? 2 : inSampleSize * 2;
+        }
+        options.inSampleSize = inSampleSize;
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeByteArray(data, 0, data.length, options);
+    }
 
     /**
-     * Encrypt using SHA-256 & AES
+     * Decode to bytes to bitmap without any downscaling
      *
      * @param data image data
-     * @param pw   password
-     * @return encrypted byte[]
+     * @return decoded bitmap
      */
-    public static byte[] encrypt(byte[] data, String pw) {
-        SecretKeySpec keySpec = createKey(pw);
-        try {
-            Cipher cipher = Cipher.getInstance(ENCRYPTION_STANDARD);
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-            byte[] encryptedData = cipher.doFinal(data);
-            return encryptedData;
-        } catch (NoSuchAlgorithmException e) {
-            Log.w("encrypt", e.toString() + ":" + e.getMessage());
-        } catch (NoSuchPaddingException e) {
-            Log.w("encrypt", e.toString() + ":" + e.getMessage());
-        } catch (InvalidKeyException e) {
-            Log.w("encrypt", e.toString() + ":" + e.getMessage());
-        } catch (BadPaddingException e) {
-            Log.w("encrypt", e.toString() + ":" + e.getMessage());
-        } catch (IllegalBlockSizeException e) {
-            Log.w("encrypt", e.toString() + ":" + e.getMessage());
-        }
-        return null;
+    public static Bitmap decode(byte[] data) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        return BitmapFactory.decodeByteArray(data, 0, data.length, options);
     }
-
-    /**
-     * Decrypt using SHA-256 & AES
-     *
-     * @param data encryptedData
-     * @param pw   password
-     * @return original data byte[]
-     */
-    public static byte[] decrypt(byte[] data, String pw) {
-        SecretKeySpec keySpec = createKey(pw);
-        try {
-            Cipher cipher = Cipher.getInstance(ENCRYPTION_STANDARD);
-            cipher.init(Cipher.DECRYPT_MODE, keySpec);
-            byte[] decryptedData = cipher.doFinal(data);
-            return decryptedData;
-        } catch (NoSuchAlgorithmException e) {
-            Log.w("encrypt", e.toString() + ":" + e.getMessage());
-        } catch (NoSuchPaddingException e) {
-            Log.w("encrypt", e.toString() + ":" + e.getMessage());
-        } catch (InvalidKeyException e) {
-            Log.w("encrypt", e.toString() + ":" + e.getMessage());
-        } catch (BadPaddingException e) {
-            Log.w("encrypt", e.toString() + ":" + e.getMessage());
-        } catch (IllegalBlockSizeException e) {
-            Log.w("encrypt", e.toString() + ":" + e.getMessage());
-        }
-        return null;
-    }
-
-    /**
-     * get the secretKeySpec through one-way hashing and encryption Algorithm (SHA-256/AES)
-     *
-     * @param pw password
-     * @return SecretKeySpec
-     */
-    private static SecretKeySpec createKey(String pw) {
-
-        try {
-            byte[] pwBytes = pw.getBytes("UTF-8");
-
-            // Create secret Key factory based on the specified algorithm
-            MessageDigest md = MessageDigest.getInstance(HASHING_ALGORITHM);
-
-            // digest the pwBytes to be a new key
-            md.update(pwBytes, 0, pwBytes.length);
-            byte[] key = md.digest();
-            return new SecretKeySpec(key, ENCRYPTION_STANDARD);
-        } catch (NoSuchAlgorithmException e) {
-            Log.w("createKey", e.toString() + ":" + e.getMessage());
-        } catch (UnsupportedEncodingException e) {
-            Log.w("createKey", e.toString() + ":" + e.getMessage());
-        }
-        return null;
-    }
-
-
 }
